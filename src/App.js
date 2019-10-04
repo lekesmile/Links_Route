71885fdbd8a6b9579eadddc5aceb4f6275c13747
Igunnuoda Olorunleke <s1900550@edu.bc.fi>
2019-10-04 11:35:34 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
-function App() {
+
+
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Home</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+
+const Product = ({match}) => {
+  return(
+    <div>
+    <h1>Product</h1>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/product1`}>Product1</Link>
+      </li>
+        <li>
+          <Link to={`${match.url}/product2`}>Product2</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/product3`}>Product3</Link>
+        </li>
+    </ul>
+      <Route
+        path={`${match.path}/:name`}
+        render={({ match }) => (
+          <div>
+            {' '}
+            <h3> I cliecked that link {match.params.name} </h3>
+          </div>
+        )}
+      />
+
+      <Route path={`${match.url}/:name`}> </Route>
+    </div>
+  )
+}
+
+const Projuct = ()=>{
+  return(
+    <h3>Pro page {} </h3>
+  )
+}
+
+const Nav = () => {
+  return (
+    <div className="primary">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/product">Product</Link>
+          </li>
+        </ul>
+      </nav>
+      
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/product" component={Product} />
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
